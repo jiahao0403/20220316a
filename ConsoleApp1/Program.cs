a6c42f14124ea9b566e09f4b9653c2bca6944170
jiahao0403 <albert104104@gmail.com>
2022-03-16 16:13:48 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Console.Write("請輸入分數:");
+            score = Convert.ToInt32(Console.ReadLine());
+            string result = score >= 60 ? "及格" : "不及格";
+            Console.WriteLine("同學你這次{0}",result);
+            Console.ReadLine();
+        }
+    }
+}
